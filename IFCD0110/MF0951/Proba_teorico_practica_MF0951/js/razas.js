7dd0bd5abe7f04d1fb2036f058626d9d1e45de41
Rubén Terré <72130385+rubenterre@users.noreply.github.com>
2026-01-07 14:10:35 +0100

    Update: Proba_avaliacion_MF0951

---
+++ IFCD0110/MF0951/Proba_teorico_practica_MF0951/js/razas.js
@@ -0,0 +1,27 @@
+const razas = {
+    "Protoss": {
+      "nome": "Protoss",
+      "descricion": "Durante milleiros de anos, os antigos e misteriosos protoss dominaron a súa parte da galaxia. Os guerreiros protoss non teñen igual en destreza e valor, grazas á combinación da tecnoloxía coa súa formidable forza psiónica.",
+      "imaxeFondo": "./razas/protoss_background_1600_desktop.avif",
+      "imaxeFondoMobile": "./razas/races_protoss_960_mobile.avif",
+      "iconoBotonDefault": "./icons/protoss_icon_default.avif",
+      "iconoBotonActive": "./icons/protoss_icon_active.avif"
+    },
+    "Terran": {
+      "nome": "Terran",
+      "descricion": "Prácticamente acabados de chegar ao sector Koprulu, os terran son os descendentes dunha desastrosa expedición de colonización que partiu da Terra hai séculos. Sen a avanzada tecnoloxía dos protoss nin o talento natural dos zerg, as forzas militares terran apóianse nunha variada combinación de unidades versátiles e resistentes para superar aos seus rivais.",
+      "imaxeFondo": "./razas/terran_background_1600_desktop.avif",
+      "imaxeFondoMobile": "./razas/races_terran_960_mobile.avif",
+      "iconoBotonDefault": "./icons/terran_icon_default.avif",
+      "iconoBotonActive": "./icons/terran_icon_active.avif"
+    },
+    "Zerg": {
+      "nome": "Zerg",
+      "descricion": "Liderados pola astuta Raíña de Espadas, os alieníxenas zerg teñen a intención de desatar os seus horrores pola galaxia e consumir a todo aquel que se interponga no seu camiño. Os zerg non empregan tecnoloxía para crear as súas armas, armaduras e naves estelares. No seu lugar, esas funcións cúmprenas de forma eficiente mediante adaptación biolóxica e mutación planificada.",
+      "imaxeFondo": "./razas/zerg_background_1600_desktop.avif",
+      "imaxeFondoMobile": "./razas/races_zerg_960_mobile.avif",
+      "iconoBotonDefault": "./icons/zerg_icon_default.avif",
+      "iconoBotonActive": "./icons/zerg_icon_active.avif"
+    }
+  }
+           
---
+++ IFCD0110/MF0951/Proba_teorico_practica_MF0951/js/script.js
@@ -0,0 +1,126 @@
+const razas = {
+    "Protoss": {
+        "nome": "Protoss",
+        "descricion": "Durante milleiros de anos, os antigos e misteriosos protoss dominaron a súa parte da galaxia. Os guerreiros protoss non teñen igual en destreza e valor, grazas á combinación da tecnoloxía coa súa formidable forza psiónica.",
+        "imaxeFondo": "./assets/razas/protoss_background_1600_desktop.avif",
+        "imaxeFondoMobile": "./assets/razas/races_protoss_960_mobile.avif",
+        "iconoBotonDefault": "./assets/icons/protoss_icon_default.avif",
+        "iconoBotonActive": "./assets/icons/protoss_icon_active.avif"
+    },
+    "Terran": {
+        "nome": "Terran",
+        "descricion": "Prácticamente acabados de chegar ao sector Koprulu, os terran son os descendentes dunha desastrosa expedición de colonización que partiu da Terra hai séculos. Sen a avanzada tecnoloxía dos protoss nin o talento natural dos zerg, as forzas militares terran apóianse nunha variada combinación de unidades versátiles e resistentes para superar aos seus rivais.",
+        "imaxeFondo": "./assets/razas/terran_background_1600_desktop.avif",
+        "imaxeFondoMobile": "./assets/razas/races_terran_960_mobile.avif",
+        "iconoBotonDefault": "./assets/icons/terran_icon_default.avif",
+        "iconoBotonActive": "./assets/icons/terran_icon_active.avif"
+    },
+    "Zerg": {
+        "nome": "Zerg",
+        "descricion": "Liderados pola astuta Raíña de Espadas, os alieníxenas zerg teñen a intención de desatar os seus horrores pola galaxia e consumir a todo aquel que se interponga no seu camiño. Os zerg non empregan tecnoloxía para crear as súas armas, armaduras e naves estelares. No seu lugar, esas funcións cúmprenas de forma eficiente mediante adaptación biolóxica e mutación planificada.",
+        "imaxeFondo": "./assets/razas/zerg_background_1600_desktop.avif",
+        "imaxeFondoMobile": "./assets/razas/races_zerg_960_mobile.avif",
+        "iconoBotonDefault": "./assets/icons/zerg_icon_default.avif",
+        "iconoBotonActive": "./assets/icons/zerg_icon_active.avif"
+    }
+}
+
+
+
+function cambiarRaza(raza) {
+
+        const razaSeleccionada = razas[raza];
+
+    const iconoProtoss = document.getElementById("iconoProtoss");
+    const iconoTerran = document.getElementById("iconoTerran");
+    const iconoZerg = document.getElementById("iconoZerg");
+
+    switch (raza) {
+        case "Protoss":
+
+            iconoProtoss.src = razas.Protoss.iconoBotonActive;
+            iconoTerran.src = razas.Terran.iconoBotonDefault;
+            iconoZerg.src = razas.Zerg.iconoBotonDefault;
+
+            break;
+
+        case "Terran":
+
+            iconoProtoss.src = razas.Protoss.iconoBotonDefault;
+            iconoTerran.src = razas.Terran.iconoBotonActive;
+            iconoZerg.src = razas.Zerg.iconoBotonDefault;
+
+            break;
+
+        case "Zerg":
+
+            iconoProtoss.src = razas.Protoss.iconoBotonDefault;
+            iconoTerran.src = razas.Terran.iconoBotonDefault;
+            iconoZerg.src = razas.Zerg.iconoBotonActive;
+
+            break;
+
+        default:
+            break;
+    }
+
+
+    const contedorRaza = document.getElementById("razas");
+
+
+    // Cambiar a imaxe de fondo
+
+    // Cambiar a imaxe de fondo
+    if (window.innerWidth <= 768) {
+        contedorRaza.style.backgroundImage = `url(${razaSeleccionada.imaxeFondoMobile})`;
+    } else {
+        contedorRaza.style.backgroundImage = `url(${razaSeleccionada.imaxeFondo})`;
+    }
+
+
+    // Cambiar a descrición
+    document.getElementById("tituloRaza").textContent = razaSeleccionada.nome;
+    document.getElementById("descripcionRaza").textContent = razaSeleccionada.descricion;
+
+    // Cambiar o icono do botón
+
+    /*     if (razaSeleccionada.nome === "Protoss") {
+          iconoProtoss.src = razaSeleccionada.iconoBotonActive; 
+      } else {
+          iconoProtoss.src = razas["Protoss"].iconoBotonDefault;
+      }
+      
+      if (razaSeleccionada.nome === "Terran") {
+          iconoTerran.src = razaSeleccionada.iconoBotonActive;
+      } else {
+          iconoTerran.src = razas["Terran"].iconoBotonDefault;
+      }
+      
+      if (razaSeleccionada.nome === "Zerg") {
+          iconoZerg.src = razaSeleccionada.iconoBotonActive;
+      } else {
+          iconoZerg.src = razas["Zerg"].iconoBotonDefault;
+      } */
+
+
+
+    //Pódese facer tamén cun operador ternario que fai o mesmo co condicional pero con outra sintaxe
+
+    /* 
+      iconoProtoss.src = razaSeleccionada.nome === "Protoss" ? razaSeleccionada.iconoBotonActive : razas["Protoss"].iconoBotonDefault;
+      iconoTerran.src = razaSeleccionada.nome === "Terran" ? razaSeleccionada.iconoBotonActive : razas["Terran"].iconoBotonDefault;
+      iconoZerg.src = razaSeleccionada.nome === "Zerg" ? razaSeleccionada.iconoBotonActive : razas["Zerg"].iconoBotonDefault; 
+    */
+
+}
+
+// Menú despregable do móbil
+
+function toggleMenu() {
+    let headerContainer = document.querySelector(".menu--mobile");
+    if (headerContainer.style.display === "block") {
+        headerContainer.style.display = "none";
+    } else {
+        headerContainer.style.display = "block";
+    }
+}
